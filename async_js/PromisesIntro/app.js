fc93dbdce6d8de74e884566f99f188ea7d35267d
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-26 15:26:57 -0300

    promises intro

--- async_js/PromisesIntro/app.js
+++ async_js/PromisesIntro/app.js
@@ -22,7 +22,7 @@ const fakeRequestPromise = url => {
     }, delay);
   });
 };
-
+// Using callbacks
 fakeRequestCallback(
   'books.com/page1',
   success => {
@@ -57,72 +57,21 @@ fakeRequestCallback(
   }
 );
 
-// fakeRequestCallback('books.com/page1',
-//     function (response) {
-//         console.log("IT WORKED!!!!")
-//         console.log(response)
-//         fakeRequestCallback('books.com/page2',
-//             function (response) {
-//                 console.log("IT WORKED AGAIN!!!!")
-//                 console.log(response)
-//                 fakeRequestCallback('books.com/page3',
-//                     function (response) {
-//                         console.log("IT WORKED AGAIN (3rd req)!!!!")
-//                         console.log(response)
-//                     },
-//                     function (err) {
-//                         console.log("ERROR (3rd req)!!!", err)
-//                     })
-//             },
-//             function (err) {
-//                 console.log("ERROR (2nd req)!!!", err)
-//             })
-//     }, function (err) {
-//         console.log("ERROR!!!", err)
-//     })
-
-// fakeRequestPromise('yelp.com/api/coffee/page1')
-//     .then(() => {
-//         console.log("IT WORKED!!!!!! (page1)")
-//         fakeRequestPromise('yelp.com/api/coffee/page2')
-//             .then(() => {
-//                 console.log("IT WORKED!!!!!! (page2)")
-//                 fakeRequestPromise('yelp.com/api/coffee/page3')
-//                     .then(() => {
-//                         console.log("IT WORKED!!!!!! (page3)")
-//                     })
-//                     .catch(() => {
-//                         console.log("OH NO, ERROR!!! (page3)")
-//                     })
-//             })
-//             .catch(() => {
-//                 console.log("OH NO, ERROR!!! (page2)")
-//             })
-//     })
-//     .catch(() => {
-//         console.log("OH NO, ERROR!!! (page1)")
-//     })
-
 // THE CLEANEST OPTION WITH THEN/CATCH
 // RETURN A PROMISE FROM .THEN() CALLBACK SO WE CAN CHAIN!
 
-/* fakeRequestPromise('yelp.com/api/coffee/page1')
+fakeRequestPromise('tibia.com/page1')
   .then(data => {
-    console.log('IT WORKED!!!!!! (page1)');
     console.log(data);
-    return fakeRequestPromise('yelp.com/api/coffee/page2');
+    return fakeRequestPromise('tibia.com/page2');
   })
   .then(data => {
-    console.log('IT WORKED!!!!!! (page2)');
     console.log(data);
-    return fakeRequestPromise('yelp.com/api/coffee/page3');
+    return fakeRequestPromise('tibia.com/page3');
   })
   .then(data => {
-    console.log('IT WORKED!!!!!! (page3)');
     console.log(data);
   })
   .catch(err => {
-    console.log('OH NO, A REQUEST FAILED!!!');
     console.log(err);
   });
- */
